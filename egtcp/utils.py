82aa77595b622740fcd70cad500c0708974e7536
hbprotoss <gamespy1991@gmail.com>
2018-01-03 16:03:20 +0800

    deep_merge_dict

--- egtcp/utils.py
+++ egtcp/utils.py
@@ -21,3 +21,28 @@ def to_dict(obj, class_prefix=None):
     else:
         ret = obj
     return ret
+
+
+def deep_merge_dict(src, dest):
+    """
+    从src复制到dest
+    对于src中到value，如果dest中不存在，直接添加；如果dest中存在，再次执行deep_merge_dict
+    :param src:
+    :param dest:
+    :return:
+    """
+    if not isinstance(src, dict) or not isinstance(dest, dict):
+        raise ValueError("src and dest must be dict")
+
+    for key, value in src.items():
+        if key not in dest:
+            dest[key] = value
+            continue
+        dest_value = dest[key]
+        if dest_value.__class__ != value.__class__:
+            raise ValueError("field '%s' in src and dest must be of the same type", key)
+
+        if isinstance(value, dict):
+            deep_merge_dict(value, dest_value)
+        else:
+            dest[key] = value
