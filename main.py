0033623642757d0e8af6db816165d119ebd7449a
hbprotoss <gamespy1991@gmail.com>
2018-01-17 09:38:09 +0800

    jobs

--- main.py
+++ main.py
@@ -2,4 +2,5 @@
 # coding=utf-8
 
 from scrapy import cmdline
-cmdline.execute("scrapy crawl global_source".split())
+
+cmdline.execute("scrapy crawl global_source -s JOBDIR=jobs/global-source-1".split())
