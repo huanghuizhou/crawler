74c5a456bf8ca05810324495acebb44e876ebdea
huanghuizhou <1017981449@qq.com>
2018-03-16 17:17:39 +0800

    dao  uodate

--- egtcp/dao_1.py
+++ egtcp/dao_1.py
@@ -7,7 +7,7 @@ from pymongo import MongoClient
 DB_NAME = 'dadaoDb'
 # COLLECTION_NAME = 'GlobalSourceSuppliers'
 COLLECTION_NAME_A = 'TradeWow'
-COLLECTION_NAME_B = 'TradeWow_1123123'
+COLLECTION_NAME_B = 'TradeWow_copy'
 
 # MONGO_CLIENT_DEV = MongoClient(host='192.168.2.203', port=27017, username="gt_rw", password="greattao5877",
 #                                authSource=DB_NAME,
@@ -15,6 +15,7 @@ COLLECTION_NAME_B = 'TradeWow_1123123'
 
 MONGO_CLIENT_LOCAL = MongoClient(host='localhost', port=27017,
                                  authSource=DB_NAME)
+
 MONGO_CLIENT_DEV = MongoClient(host='localhost', port=27017,
                                authSource=DB_NAME)
 
